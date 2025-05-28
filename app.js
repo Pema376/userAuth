7639798e3945999d93fc00085e0c0bb83501dca7
Pema376 <cpema455@gmail.com>
2025-05-28 11:14:51 +0600

    Add adminRoutes.js

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-require('dotenv').config();
-const { createUserTable } = require('./models/usermodel');
-
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+require("dotenv").config();
+const { createUserTable } = require("./models/usermodel");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 
 // Middlewares
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: process.env.SESSION_SECRET || 'secretkey',
-resave: false,
-  saveUninitialized: true,
-}));
-
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "secretkey",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 // View engine
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Routes
-const adminRoutes = require('./routes/adminRoutes');
-const authRoutes = require('./routes/authroutes');
-const userRoutes = require('./routes/userRoutes');
-app.use('/', authRoutes);
-app.use('/admin', adminRoutes);
-app.use('/user', userRoutes);
-
-
+const adminRoutes = require("./routes/adminroutes");
+const authRoutes = require("./routes/authroutes");
+const userRoutes = require("./routes/userRoutes");
+app.use("/", authRoutes);
+app.use("/admin", adminRoutes);
+app.use("/user", userRoutes);
 
 // Schema creation
-const { createFoodTable } = require('./models/foodModel');
-createUserTable()
+const { createFoodTable } = require("./models/foodModel");
+createUserTable();
 createFoodTable();
-
 
 // Server
 app.listen(PORT, () => {
